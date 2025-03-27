23d46f60ecd9b929de5c70519ddcec7a08767d1e
DanielContrerasCh <danicontreras0409@gmail.com>
2025-03-27 13:49:07 -0600

    Crear la estructura inicial
    Se creo la estructura inicial del backend desacoplado, agregando
    configuracion inicial basica de app.js, rutas y carpetas.

---
+++ app.js
@@ -0,0 +1,39 @@
+require("dotenv").config();
+const express = require("express");
+
+const app = express();
+const puerto = process.env.PUERTO || 8080;
+
+const bodyParser = require("body-parser");
+
+app.use(
+    bodyParser.urlencoded({
+      extended: false,
+    })
+  );
+app.use(bodyParser.json());
+
+const sesionRutas = require("./sesion/rutas/sesionIndice.rutas");
+const reportesRutas = require("./reportes/rutas/reportesIndice.rutas");
+const formulasRutas = require("./formulas/rutas/formulasIndice.rutas");
+
+app.use("/sesion", sesionRutas);
+app.use("/reportes", reportesRutas);
+app.use("/formulas", formulasRutas);
+
+app.get("/", (pet, res) => {
+    res.status(200).json({
+      message: "¡Bienvenido a Saca la Bici!",
+    });
+  });
+
+
+app.use((pet, res) => {
+    res.status(404).json({
+      message: "No se encuentra el endpoint o ruta que estas buscando",
+    });
+  });
+
+  app.listen(puerto, () => {
+    console.log(`Servidor escuchando en el puerto ${puerto}`);
+  });
