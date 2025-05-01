b9c8e80ba88956ba1f8255e105bfcf22909d8ecf
DanielContrerasCh <danicontreras0409@gmail.com>
2025-05-01 17:39:47 -0600

    Agregar referencia a RF en middlewareAutenticacion

--- util/middlewareAutenticacion.js
+++ util/middlewareAutenticacion.js
@@ -1,4 +1,5 @@
 // RF2 Usuario registrado inicia sesión - https://codeandco-wiki.netlify.app/docs/proyectos/tractores/documentacion/requisitos/RF2
+// RF3 Usuario cierra sesión - https://codeandco-wiki.netlify.app/docs/proyectos/tractores/documentacion/requisitos/RF3
 
 const jwt = require('jsonwebtoken');
 const listaNegra = require('./listaNegra');
