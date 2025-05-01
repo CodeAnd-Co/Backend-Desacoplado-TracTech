02b12731ab733257f655ecf23f59e3d8710978d9
Rommel <rommtoledo@gmail.com>
2025-05-01 15:32:06 -0600

    Añadir middleware de permisos para la ruta de consulta de usuarios

--- usuarios/rutas/consultarUsuarios.ruta.js
+++ usuarios/rutas/consultarUsuarios.ruta.js
@@ -3,7 +3,8 @@ const ruteador = express.Router();
 const verificarToken = require("../../util/middlewareAutenticacion");
 
 const consultarUsuariosControlador = require('../controladores/consultarUsuarios.controlador');
+const { verificarPermisos, checarPermisos } = require('../../util/middlewarePermisos');
 
-ruteador.get("/", verificarToken, consultarUsuariosControlador.consultarUsuariosControlador);
+ruteador.get("/", verificarToken, verificarPermisos, checarPermisos('ADMIN'), consultarUsuariosControlador.consultarUsuariosControlador);
 
 module.exports = ruteador;
