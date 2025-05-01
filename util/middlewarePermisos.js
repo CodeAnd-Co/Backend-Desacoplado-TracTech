ed1544b0f5b3e0538959489d3e7a00cf17324961
Rommel <rommtoledo@gmail.com>
2025-05-01 15:31:49 -0600

    Añadir verificación individual de permisos

--- util/middlewarePermisos.js
+++ util/middlewarePermisos.js
@@ -4,13 +4,13 @@ const conexion = require("../util/bd");
  * Middleware para verificar los permisos de un usuario basándose en su ID obtenido del token JWT.
  * Llama a un procedimiento almacenado para recuperar los permisos del usuario y los adjunta a la petición.
  * 
- * @param {object} pet - Objeto de solicitud (request) de Express
- * @param {object} res - Objeto de respuesta (response) de Express
+ * @param {object} peticion - Objeto de solicitud (request) de Express
+ * @param {object} respuesta - Objeto de respuesta (response) de Express
  * @param {function} siguiente - Función next() para pasar al siguiente middleware
  */
-const verificarPermisos = (pet, res, siguiente) => {
+const verificarPermisos = (peticion, respuesta, siguiente) => {
     // Obtiene el ID del usuario desde el objeto usuario (ya decodificado del token)
-    const idUsuario = pet.usuario.id;
+    const idUsuario = peticion.usuario.id;
 
     try {
         // Definir la consulta al procedimiento almacenado para obtener permisos
@@ -21,11 +21,11 @@ const verificarPermisos = (pet, res, siguiente) => {
             if (error) {
                 // Si ocurre un error al ejecutar el stored procedure, retornar error 500
                 console.error('Error al ejecutar el stored procedure:', error);
-                return res.status(500).json({ message: "Error interno del servidor" });
+                return respuesta.status(500).json({ message: "Error interno del servidor" });
             }
 
             // Asignar los permisos obtenidos al objeto de solicitud
-            pet.permisos = resultados[0];
+            peticion.permisos = resultados[0].map(resultado => resultado.permiso);
 
             // Llamar al siguiente middleware en la cadena
             siguiente();
@@ -33,8 +33,19 @@ const verificarPermisos = (pet, res, siguiente) => {
     } catch (error) {
         // Capturar cualquier error inesperado y devolver error 500
         console.error('Error al verificar permisos:', error);
-        return res.status(500).json({ message: "Error interno del servidor" });
+        return respuesta.status(500).json({ message: "Error interno del servidor" });
     }
 }
 
-module.exports = verificarPermisos;
+const checarPermisos = (permiso) => {
+    return (peticion, respuesta, siguiente) => {
+        if (!peticion.permisos.includes(permiso)) {
+            return respuesta.status(404).json({
+                message: "Página no encontrada",
+            });
+        }
+        siguiente();
+    }
+}
+
+module.exports = { verificarPermisos, checarPermisos };
