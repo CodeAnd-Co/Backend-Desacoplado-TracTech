e3020bc90df8f5ee6b3e53e0a2408646e3a9a9e5
DanielContrerasCh <danicontreras0409@gmail.com>
2025-05-01 17:53:51 -0600

    Importar solo Router() de express

--- sesion/rutas/cerrarSesion.ruta.js
+++ sesion/rutas/cerrarSesion.ruta.js
@@ -1,10 +1,11 @@
 // RF3 Usuario cierra sesión - https://codeandco-wiki.netlify.app/docs/proyectos/tractores/documentacion/requisitos/RF3
 
-const express = require('express');
-const ruteador = express.Router();
+const { Router } = require('express');
 const verificarToken = require('../../util/middlewareAutenticacion');
 
 const cerrarSesionControlador = require('../controladores/cerrarSesion.controlador');
+
+const ruteador = Router();
 
 ruteador.post('/', verificarToken, cerrarSesionControlador.cerrarSesion);
 
--- sesion/rutas/sesionIndice.rutas.js
+++ sesion/rutas/sesionIndice.rutas.js
@@ -1,11 +1,11 @@
 // RF2 Usuario registrado inicia sesión - https://codeandco-wiki.netlify.app/docs/proyectos/tractores/documentacion/requisitos/RF2
 // RF3 Usuario cierra sesión - https://codeandco-wiki.netlify.app/docs/proyectos/tractores/documentacion/requisitos/RF3
 
-const express = require('express');
+const { Router } = require('express');
 const iniciarSesionRuta = require('./iniciarSesion.ruta');
 const cerrarSesionRuta = require('./cerrarSesion.ruta');
 
-const ruteador = express.Router();
+const ruteador = Router();
 
 ruteador.use('/iniciar-sesion', iniciarSesionRuta);
 ruteador.use('/cerrar-sesion', cerrarSesionRuta);
