78fcda4b06df3f6ce20eb21b9fe74af7c40ce4ba
ELECTRIC80008STORM <63619664+ELECTRIC80008STORM@users.noreply.github.com>
2025-05-06 16:09:22 -0600

    Añadir trazabilidad con US41 Admin Modifica Usuario

--- usuarios/data/modelos/usuarios.js
+++ usuarios/data/modelos/usuarios.js
@@ -1,4 +1,5 @@
 // RF40 Administrador consulta usuarios - https://codeandco-wiki.netlify.app/docs/proyectos/tractores/documentacion/requisitos/RF40
+// RF41 Administrador modifica usuario - https://codeandco-wiki.netlify.app/docs/proyectos/tractores/documentacion/requisitos/RF41
 
 class Usuario {
   constructor({ id, nombre, correo}) {
