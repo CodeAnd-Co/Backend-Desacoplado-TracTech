ba836b0f73f6c545a0ceb941564c9f78d1232ed3
ELECTRIC80008STORM <63619664+ELECTRIC80008STORM@users.noreply.github.com>
2025-05-06 16:10:57 -0600

    Añadir ruta para RF41 Admin Modifica Usuario

--- usuarios/rutas/usuariosIndice.rutas.js
+++ usuarios/rutas/usuariosIndice.rutas.js
@@ -1,13 +1,16 @@
 // RF39: Administrador crea usuario - https://codeandco-wiki.netlify.app/docs/proyectos/tractores/documentacion/requisitos/RF39
 // RF40 Administrador consulta usuarios - https://codeandco-wiki.netlify.app/docs/proyectos/tractores/documentacion/requisitos/RF40
+// RF41 Administrador modifica usuario - https://codeandco-wiki.netlify.app/docs/proyectos/tractores/documentacion/requisitos/RF41
 
 const express = require('express');
 const ruteador = express.Router();
 
 const consultarUsuariosRuta = require('./consultarUsuarios.ruta');
 const crearUsuarioRuta = require('./crearUsuario.ruta');
+const modificarUsuarioRuta = require('./modificarUsuario.ruta');
 
 ruteador.use('/consultar-usuarios', consultarUsuariosRuta);
 ruteador.use('/crear-usuario', crearUsuarioRuta);
+ruteador.use('/modificar-usuario', modificarUsuarioRuta);
 
 module.exports = ruteador;
