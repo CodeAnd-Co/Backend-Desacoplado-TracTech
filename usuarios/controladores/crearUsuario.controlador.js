d1068838791f78c7751688fdf5bb86d109469e29
DanielQueijeiro <dany.queijeiro@hotmail.com>
2025-05-11 13:09:36 -0600

    Quitar console.logs

--- usuarios/controladores/crearUsuario.controlador.js
+++ usuarios/controladores/crearUsuario.controlador.js
@@ -11,11 +11,8 @@ const { crearUsuarioRepositorio } = require('../data/repositorios/usuarios.repos
  * @throws {Error} Error interno del servidor al procesar la petición
  */
 exports.crearUsuarioControlador = async (peticion, respuesta) => {
-  console.log('controlador crearUsuarioControlador');
   try {
-    /* eslint-disable-next-line camelcase */
     const { nombre, correo, contrasenia, idRolFK } = peticion.body;
-    /* eslint-disable-next-line camelcase */
     if (!nombre || !correo || !contrasenia || !idRolFK) {
       return respuesta.status(400).json({
         mensaje: 'Un campo requerido está vacío',
@@ -25,12 +22,10 @@ exports.crearUsuarioControlador = async (peticion, respuesta) => {
     // Cifrar la contraseña antes de guardarla
     const rondasSalteadas = 12; // Número de rondas de sal
     const contraseniaCifrada = await bcrypt.hash(contrasenia, rondasSalteadas);
-    console.log('Contraseña cifrada:', contraseniaCifrada);
 
     // Llamar al repositorio con la contraseña cifrada
      
     const idInsertado = await crearUsuarioRepositorio(nombre, correo, contraseniaCifrada, idRolFK);
-    console.log('ID del usuario insertado:', idInsertado);
 
     respuesta.status(201).json({
       mensaje: 'Usuario creado exitosamente',
