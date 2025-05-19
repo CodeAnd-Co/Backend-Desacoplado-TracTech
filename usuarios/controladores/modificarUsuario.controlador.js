e55a3933fce7eadcdbc56c9e71ea888186872f78
Rommel <rommtoledo@gmail.com>
2025-05-19 02:02:07 -0600

    Eliminar import de validator innecesario

--- usuarios/controladores/modificarUsuario.controlador.js
+++ usuarios/controladores/modificarUsuario.controlador.js
@@ -3,7 +3,6 @@
 const { modificarUsuario: modificarUsuarioRepositorio } = require('../data/repositorios/usuarios.repositorio.js');
 const { validarYLimpiarUsuario } = require('./validacionesCompartidas.js');
 const bcrypt = require('bcrypt');
-const validator = require('validator');
 
 /**
  * Esta función valida y sanitiza los datos recibidos desde el cuerpo de la petición HTTP.
