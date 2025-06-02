740e383adbbb7bb83e5582a2dedc35d7b6e488fc
ELECTRIC80008STORM <63619664+ELECTRIC80008STORM@users.noreply.github.com>
2025-06-02 13:00:13 -0600

    Procesar el resultado del stored procedure

--- usuarios/data/repositorios/consultarUsuariosRepositorio.js
+++ usuarios/data/repositorios/consultarUsuariosRepositorio.js
@@ -1,16 +1,24 @@
 const modelo = require('../modelos/consultarUsuariosModelo.js');
 
-function consultarUsuarios() {
-    const rolAExcluir = process.env.SU;
-    const datos = modelo.consultarUsuarios(rolAExcluir);
-    if (!datos || datos.length === 0) {
-        return {
-            estado: 404,
-            mensaje: 'No se encontraron usuarios',
-        };
+async function consultarUsuarios() {
+    try {
+        const rolAExcluir = process.env.SU;
+        const datos = await modelo.consultarUsuarios(rolAExcluir);
+
+        const usuarios = datos[0];
+        
+        if (!usuarios || usuarios.length === 0) {
+            return {
+                estado: 404,
+                mensaje: 'No se encontraron usuarios',
+            };
+        }
+        return usuarios;
+    } catch (error) {
+        console.error('Error en consultarUsuariosRepositorio:', error);
+        throw error;
     }
-    return datos;
-  }
+}
 
 module.exports = {
     consultarUsuarios,
