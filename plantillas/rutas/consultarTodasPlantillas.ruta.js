b251af390825d49a10052cde918ceb25a8b0b65a
July230 <A01352823@tec.mx>
2025-06-16 19:16:37 -0600

    Corregir defectos de trazabilidad en rutas de plantillas

--- plantillas/rutas/consultarTodasPlantillas.ruta.js
+++ plantillas/rutas/consultarTodasPlantillas.ruta.js
@@ -1,3 +1,5 @@
+//RF32 Usuario consulta plantillas de reporte. - https://codeandco-wiki.netlify.app/docs/next/proyectos/tractores/documentacion/requisitos/RF32
+
 const express = require('express');
 const ruteador = express.Router();
 
